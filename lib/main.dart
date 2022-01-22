a7da850dea9f4d8feebb48d6f24d2935c1fc2193
FrankBonanno <fb0023@mix.wvu.edu>
2022-01-21 23:12:51 -0500

    test

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text('Flutter is confusing!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Pressed!');
+          },
+        ),
+      ),
+    );
+  }
+}
